8c45cc9c91abf7ad83904cf31517a1bd065b2110
L1S21BSSE0017 <L1S21BSSE0017@ucp.com>
2024-02-01 12:21:58 +0500

    feature user-authentication added

--- E-Learning.cpp
+++ E-Learning.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include<String>
+using namespace std;
+
+
+class ELearning{
+private : 
+String students;
+String Teachers;
+String Courses;
+public:
+ELearning();
+
+void userAuthentication(String student){
+
+if(user==student && pass == password){
+cout<< "student authenticated" <<endl;
+}
+}
+void courseManagement();
+void studentEnrollment();
+void discussionForums();
+void liveSessions();
+void gradingStudents();
+void finalizeResults();
+
+};
+
+int main(){
+
+return 0;
+
+}
+
 
